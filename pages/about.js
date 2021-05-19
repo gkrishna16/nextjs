e4be17dd4dda98def76098da02603aeeef3a9724
Gopal <gkrishna160@gmail.com>
2021-05-19 12:33:32 -0400

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        loremIn sint sunt deserunt adipisicing enim magna dolor. Exercitation
+        reprehenderit aute enim amet ex fugiat ea et aliquip do labore cillum
+        ullamco.
+      </p>{" "}
+      <p>
+        loremIn sint sunt deserunt adipisicing enim magna dolor. Exercitation
+        reprehenderit aute enim amet ex fugiat ea et aliquip do labore cillum
+        ullamco.
+      </p>
+    </div>
+  );
+};
+
+export default About;
